f6f25adb99be26774f057162ad18d8e572ccbc1f
Adam Hruška <adam.hruska.s@gyarab.cz>
2025-12-04 09:22:20 +0100

    4.12.

--- src/JDBC/MojeDatabaze.java
+++ src/JDBC/MojeDatabaze.java
@@ -1,40 +1,45 @@
 package JDBC;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
-public class MojeDatabaze {
+    public class MojeDatabaze {
     public static void main(String[] args) {
-        List<Clovek> lidi = new ArrayList<>();
-            lidi.add(new Clovek("Jan", "Lana", 3));
-            lidi.add(new Clovek("Adam", "Hruska", 4));
-            lidi.add(new Clovek("Pepa", "Zdepa", 5));
+        ArrayList<Clovek> lidi = new ArrayList<>();
+        lidi.add(new Clovek("Jana", "Nova", 3));
+        lidi.add(new Clovek("Maria", "Lanova", 4));
+        lidi.add(new Clovek("Pepa", "Zdepa", 5));
 
         System.out.println(lidi);
 
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/adam",null,null);
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute(" CREATE TABLE IF NOT EXISTS clovek ( jmeno VARCHAR, primeni VARCHAR, rocnik INTEGER)");
+            }
+
+            String sql = "INSERT INTO clovek (jmeno, primeni, rocnik) VALUES (?, ?, ?)";
+            try (PreparedStatement ps = conn.prepareStatement(sql)) {
+                for (Clovek c : lidi) {
+                    ps.setString(1, c.getJmeno());
+                    ps.setString(2, c.getPrijmeni());
+                    ps.setInt(3, c.getRocnik());
+                    ps.executeUpdate();
+                }
+            }
+
+            try (Statement stmt = conn.createStatement()) {
+                ResultSet rs = stmt.executeQuery("SELECT * FROM clovek");
+                while (rs.next()) {
+                    System.out.println(
+                            rs.getString("jmeno") + " "
+                                    + rs.getString("primeni") + " "
+                                    + rs.getInt("rocnik"));
+                }
+            }
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-
-        try {
-            Statement statement = connection.createStatement();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        String sql = "INSERT INTO clovek(jmeno, prijmeni, rocnik) VALUES ('"
-                + lidi.get(0).getJmeno() + "', '"
-                + lidi.get(0).getPrijmeni() + "',"
-                + lidi.get(0).getRocnik() + ")";
-
-        System.out.println(sql);
-
     }
 }
