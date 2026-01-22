e50698b3b66b7f93f89c9a7f15004ba57abddde7
Adam Hruška <adam.hruska.s@gyarab.cz>
2026-01-22 09:16:48 +0100

    22.1.

---
+++ src/Grafy/Hrana.java
@@ -0,0 +1,16 @@
+public class Hrana {
+    private final Vrchol vrcholA;
+    private final Vrchol vrcholB;
+
+    public Hrana(Vrchol vrcholA, Vrchol vrcholB) {
+        this.vrcholA = vrcholA;
+        this.vrcholB = vrcholB;
+    }
+
+    public Vrchol getVrcholA() {
+        return vrcholA;
+    }
+    public Vrchol getVrcholB() {
+        return vrcholB;
+    }
+}
---
+++ src/Grafy/Vrchol.java
@@ -0,0 +1,11 @@
+public class Vrchol {
+    private String nazev;
+
+    public Vrchol(String nazev) {
+        this.nazev = nazev;
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+}
